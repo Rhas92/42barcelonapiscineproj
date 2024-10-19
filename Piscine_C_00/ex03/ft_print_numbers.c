1438b0c4cbffd096b05df4f5bbd545428b46ddeb
rhas <ramonaller@gmail.com>
2024-10-20 01:47:06 +0200

    corrected Ejercicio 03.

--- Piscine_C_00/ex03/ft_print_numbers.c
+++ Piscine_C_00/ex03/ft_print_numbers.c
@@ -1,18 +1,12 @@
 #include <unistd.h>
 
-void ft_print_numbers(void)
+void	ft_print_numbers(void)
 {
 	char numb = '0';
+	
 	while (numb <= '9')
        	{
 		write(1, &numb, 1);
 		numb++;
 	}
-}
-
-int main(void)
-{
-	ft_print_numbers();
-	write(1, "\n", 1);
-	return 0;
 }
