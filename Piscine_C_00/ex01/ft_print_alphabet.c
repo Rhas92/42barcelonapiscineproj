2265c7a9b8afa72e7457099d8661b5db6dd95d6d
rhas <ramonaller@gmail.com>
2024-10-19 23:32:50 +0200

    Finished Ejercicio 01.

---
+++ Piscine_C_00/ex01/ft_print_alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char letter = 'a';
+	while (letter <= 'z')
+	{
+		write(1, &letter, 1);
+		letter++;
+	}
+}
+
+int	main()
+{
+	ft_print_alphabet();
+	write(1, "\n", 1);
+	return 0;
+}
