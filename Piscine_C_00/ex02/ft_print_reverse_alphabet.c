39edd47f2193b85493eeb6934702a0fd08f5dc82
rhas <ramonaller@gmail.com>
2024-10-20 01:43:03 +0200

    corrected Ejercicio 02.

--- Piscine_C_00/ex02/ft_print_reverse_alphabet.c
+++ Piscine_C_00/ex02/ft_print_reverse_alphabet.c
@@ -1,19 +1,13 @@
 #include <unistd.h>
 
-// Loop to write each letter from the 'a' to the 'z'
+// Loop to write each letter from the 'z' to the 'a'
 void	ft_print_reverse_alphabet(void)
 {
 	char letter = 'z';
+
 	while (letter >= 'a')
 	{
 		write(1, &letter, 1);
 		letter--;
 	}
-}
-
-int	main()
-{
-	ft_print_reverse_alphabet();
-	write(1, "\n", 1);
-	return 0;
 }
